bd080c732de55d6137365d4f3b4efe8cb7eabfb8
lemon-piepie <purplechilde@163.com>
2020-08-03 20:02:16 +0800

    reforming OrderReceipt.java.

--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -8,10 +8,10 @@ package com.twu.refactoring;
  * 
  */
 public class OrderReceipt {
-    private Order o;
+    private Order order;
 
-    public OrderReceipt(Order o) {
-        this.o = o;
+    public OrderReceipt(Order order) {
+        this.order = order;
 	}
 
 	public String printReceipt() {
@@ -21,15 +21,15 @@ public class OrderReceipt {
 		output.append("======Printing Orders======\n");
 
 		// print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
+//      output.append("Date - " + order.getDate();
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
+//      output.append(order.getCustomerLoyaltyNumber());
 
 		// prints lineItems
 		double totSalesTx = 0d;
-		double tot = 0d;
-		for (LineItem lineItem : o.getLineItems()) {
+		double total = 0d;
+		for (LineItem lineItem : order.getLineItems()) {
 			output.append(lineItem.getDescription());
 			output.append('\t');
 			output.append(lineItem.getPrice());
@@ -39,19 +39,18 @@ public class OrderReceipt {
 			output.append(lineItem.totalAmount());
 			output.append('\n');
 
-			// calculate sales tax @ rate of 10%
-            double salesTax = lineItem.totalAmount() * .10;
+			double rate = 0.10;
+            double salesTax = lineItem.totalAmount() * rate;
             totSalesTx += salesTax;
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            tot += lineItem.totalAmount() + salesTax;
+            total += lineItem.totalAmount() + salesTax;
 		}
 
 		// prints the state tax
 		output.append("Sales Tax").append('\t').append(totSalesTx);
 
         // print total amount
-		output.append("Total Amount").append('\t').append(tot);
+		output.append("Total Amount").append('\t').append(total);
 		return output.toString();
 	}
 }
